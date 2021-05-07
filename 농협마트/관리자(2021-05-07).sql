aa1fca8a4dad2f86f5c4954082f9ce71c33f3ef8
wjdfbdns819 <wjdfbdns819@wjdfbdns819.com>
2021-05-07 16:37:50 +0900

    DB 수업_05_07

---
+++ 농협마트/관리자(2021-05-07).sql
@@ -0,0 +1,11 @@
+-- 관리자 접속
+
+CREATE TABLESPACE nonghyupDB
+DATAFILE 'C:/oraclexe/data/nonghyup.dbf'
+SIZE 1M AUTOEXTEND ON NEXT 1K;
+
+CREATE USER nhuser IDENTIFIED BY nhuser
+DEFAULT TABLESPACE nonghyupDB;
+
+GRANT DBA to nhuser;
+
